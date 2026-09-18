c1e52d0d9c2c72ee2ed4ba674b5a65fddcd759c5
CaptainGold1 <48659572+CaptainGold1@users.noreply.github.com>
2026-09-17 19:04:52 -0500

    Fix search inputs in All Charts / Look Up not working with Windows Japanese IME (#1)
    * Fix search inputs in All Charts / Look Up not working with Windows
    Japanese IME
    
    * Added role="searchbox" for accessibility

--- web/components/dash/Lookup.tsx
+++ web/components/dash/Lookup.tsx
@@ -108,7 +108,8 @@ export function Lookup({ target }: { target: LookupTarget | null }) {
       <div className="search-row">
         <input
           className="search"
-          type="search"
+          type="text"
+          role="searchbox"
           placeholder="title, artist or charter"
           value={query}
           onChange={(e) => search(e.target.value)}
